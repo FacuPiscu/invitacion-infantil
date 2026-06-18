9b665b33ddb98c946d03dae7991c63c3b77c61b7
Facundo <FacuPiscu@users.noreply.github.com>
2026-06-18 14:20:12 -0300

    Revert "add: PNGs decorativos con mix-blend-mode, mask-image y filter para integracion visual"
    This reverts commit 2e49004df8a2e3b6bda7f0e2e35a3461f10712b1.

--- src/componentes/HeaderMagico.jsx
+++ src/componentes/HeaderMagico.jsx
@@ -8,8 +8,6 @@ export default function HeaderMagico({ imgSrc }) {
       <div className={estilos.gradiente} />
       <div className={estilos.centelleos} />
       <div className={estilos.polvoHadas} />
-      <img src="/paleta/blancanieves-recorte.png" alt="" className={estilos.marcaAgua} />
-      <img src="/paleta/pngegg (12).png" alt="" className={estilos.decEsquinaSup} />
 
       <div className={estilos.contenido}>
         <div className={estilos.iconos}>
